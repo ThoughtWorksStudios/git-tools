737b0c3a9f15d1e1ad07302ca68cbe9ee91058b1
malee <malee@thoughtworks.com>
2012-01-13 15:54:57 -0800

    provide better way to pull out author and email, colorize output

--- hooks/enforce-username-pre-commit.rb
+++ hooks/enforce-username-pre-commit.rb
@@ -5,16 +5,28 @@
 # this by using Ruby.
 
 def die(msg, code=1)
-  puts msg
+  puts red(msg)
   exit code
 end
 
-current_author, current_email=`git var GIT_AUTHOR_IDENT`.split[0..1]
+def red(msg)
+  "#{`tput setaf 1`}#{msg}#{`tput sgr0`}"
+end
+
+def green(msg)
+  "#{`tput setaf 2`}#{msg}#{`tput sgr0`}"
+end
+
+`git var GIT_AUTHOR_IDENT` =~ /^([^<]+)<([^>]+)/
+current_author, current_email = $1.strip, $2.strip
 
 die "You can't be #{current_author}. Please set your username with git-config" if [ENV["SUDO_USER"], ENV["USER"], "tworker"].compact.include? current_author
 die "Your email can't start with 'tworker'. Please set your email with git-config" if current_email.start_with? "tworker"
 
-print "Current developer is set to: #{current_author} #{current_email}.\n  Is this ok? [y/n] "
+print %Q{
+Current developer is set to: #{green current_author} <#{green current_email}>
+  Is this ok? [y/n]: }
+
 answer = File.new("/dev/tty").readline.chomp.downcase
 
 die "ABORT: Please set your username and email with git-config." unless answer == "y"
