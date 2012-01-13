271b3cc8192fa2273e402e8db4325eff73047690
malee <malee@thoughtworks.com>
2012-01-12 16:03:18 -0800

    remove bash_boost in favor of simpler, git-specific profile prompt

--- profile/bash_profile_enhancements.sh
+++ profile/bash_profile_enhancements.sh
@@ -1,9 +1,28 @@
 # git bash completion
 if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
-      . /usr/local/git/contrib/completion/git-completion.bash
+  . /usr/local/git/contrib/completion/git-completion.bash
 fi
 
-bash_boost=$HOME/git-tools/profile/bash_boost
-. $bash_boost/bash_colors.sh
-. $bash_boost/bash_vcs.sh
-. $bash_boost/profile_prompt.sh
+readonly _reset=$(tput sgr0)
+readonly _red=$(tput setaf 1)
+readonly _green=$(tput setaf 2)
+
+function has_changes {
+  local status=$(git status --porcelain 2>/dev/null | grep -vE '^\?\? ')
+  if [ -n "$status" ]; then
+    echo -n $_red
+  else
+    echo -n $_green
+  fi
+}
+
+function git_user_name {
+  local author=$(git config --get user.name)
+  if [ -z "$author" ]; then
+    echo -n "${_red}user.name IS UNSET${_reset}"
+  else
+    echo -n $author
+  fi
+}
+
+PS1='\h:\W $(__git_ps1 "($(has_changes)%s, $(git_user_name)$_reset) ")\$ '
